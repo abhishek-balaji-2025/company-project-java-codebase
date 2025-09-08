c0733b254a21389e2c86d97aee2719f53864386f
Abhishek Balaji <abhishekbalaji2019@gmail.com>
2025-09-08 13:21:00 +0530

    developer has made the necessary code commits and resources to the repo

---
+++ src/main/java/com/company/project/MainApp.java
@@ -0,0 +1,7 @@
+package com.company.project;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Company Project Java Codebase");
+    }
+}
---
+++ src/test/java/com/company/project/MainAppTest.java
@@ -0,0 +1,12 @@
+package com.company.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainAppTest {
+    @Test
+    public void testAppRuns() {
+        assertTrue(true);
+    }
+}
