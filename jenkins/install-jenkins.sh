b48d90b871f57f2fe5f240bc79ea265bd672d059
Abhishek Balaji <abhishekbalaji2019@gmail.com>
2025-09-08 13:57:08 +0530

    jenkins resources are added

---
+++ jenkins/install-jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# update your current system
+sudo apt update -y && sudo apt upgrade -y
+
+# install java
+sudo apt install openjdk-17-jdk -y
+
+# Add Jenkins repository and install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+# install jenkins
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# Note: Make sure to open the port 8080 in the security group
+
+# add user jenkins
+sudo adduser jenkins
+
+
